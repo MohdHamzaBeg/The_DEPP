343beafa1ffc7936d7b976545c808763ab1a77fd
MohdHamzaBeg <hamzebeg12june@gmail.com>
2023-09-16 02:01:14 +0530

    Created a new project

---
+++ The-Dive/src/main/java/com/bca/springframework/thedive/TheDiveApplication.java
@@ -0,0 +1,13 @@
+package com.bca.springframework.thedive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDiveApplication.class, args);
+	}
+
+}
---
+++ The-Dive/src/test/java/com/bca/springframework/thedive/TheDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bca.springframework.thedive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
